9e1c69734b4013ec00b07a47ed416b6e78da1f6f
Yevgeny Shnaidman <yshnaidm@redhat.com>
2020-07-26 14:57:45 +0300

    Revert "MGMT-1318 - add annotations to BMH CRs that include data from bm-inve… (#12)"
    This reverts commit 6e5205ae08d21357c563ee258172b2023394b450.
    
    Waiting till bm-inventory change is propagated...

--- test_utils.py
+++
@@ -1,8 +0,0 @@
-import utils
-import yaml
-
-
-def get_test_list_hosts(cluster_id):
-    with open('/data/installer_dir/test_hosts_list.yaml', 'r') as file:
-        hosts_list = yaml.full_load(file)
-        return [utils.InventoryHost(host) for host in hosts_list if host['status'] != 'disabled']
