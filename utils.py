ac0b8848c104d31afe59fc4552e90b448042d808
Yevgeny Shnaidman <yshnaidm@redhat.com>
2020-08-23 01:36:37 +0300

    MGMT-1890 - assisted-service client should be backwards compatible

--- utils.py
+++ utils.py
@@ -3,50 +3,50 @@ import json
 from assisted_service_client import ApiClient, Configuration, api, models
 
 
-
 class InventoryHost:
 
     def __init__(self, host_dict):
-        self._host = models.Host(**host_dict)
-        self._inventory = models.Inventory(**json.loads(self._host.inventory))
-
+        self._host = host_dict
+        self._inventory = json.loads(host_dict['inventory'])
 
     def get_inventory_host_nics_data(self):
-        interfaces_list = [models.Interface(**interface) for interface in self._inventory.interfaces]
-        return [{'name': interface.name, 'model': interface.product, 'mac': interface.mac_address, 'ip': self._get_network_interface_ip(interface), 'speed': interface.speed_mbps} for interface in interfaces_list]
+        interfaces_list = self._inventory['interfaces']
+        return [{'name': interface.get('name', ''), 'model': interface.get('product', ''), 'mac': interface.get('mac_address'), 'ip': self._get_network_interface_ip(interface), 'speed': interface.get('speed_mbps', 0)} for interface in interfaces_list]
 
 
     def get_inventory_host_cpu_data(self):
-        cpu = models.Cpu(**self._inventory.cpu)
-        return {'model': cpu.model_name, 'arch': cpu.architecture, 'flags': cpu.flags, 'clockMegahertz': cpu.frequency, 'count': cpu.count}
+        cpu = self._inventory['cpu']
+        return {'model': cpu.get('model_name', ''), 'arch': cpu.get('architecture', ''), 'flags': cpu.get('flags', ''), 'clockMegahertz': cpu.get('frequency', 0.0), 'count': cpu.get('count', 0)}
 
 
     def get_inventory_host_storage_data(self):
-        disks_list = [models.Disk(**disk) for disk in self._inventory.disks]
-        return [{'name': disk.name, 'vendor': disk.vendor, 'sizeBytes': disk.size_bytes, 'model': disk.model, 'wwn': disk.wwn, 'hctl': disk.hctl, 'serialNumber': disk.serial, 'rotational': True if disk.drive_type == 'HDD' else False} for disk in disks_list] 
+        disks_list = self._inventory['disks']
+        return [{'name': disk.get('name', ''), 'vendor': disk.get('vendor', ''), 'sizeBytes': disk.get('size_bytes', 0), 'model': disk.get('model', ''), 'wwn': disk.get('wwn', ''), 'hctl': disk.get('hctl', None), 'serialNumber': disk.get('serial', ''), 'rotational': True if disk.get('drive_type', '') == 'HDD' else False} for disk in disks_list] 
 
 
     def get_inventory_host_memory(self):
-        memory = models.Memory(**self._inventory.memory)
-        return int(memory.physical_bytes / 1024 / 1024)
+        memory = self._inventory['memory']
+        return int(memory.get('physical_bytes', 0) / 1024 / 1024)
 
 
     def get_inventory_host_name(self):
-        return self._host.requested_hostname
+        return self._host.get('requested_hostname', '')
 
 
     def get_inventory_host_system_vendor(self):
-        system_vendor = models.SystemVendor(**self._inventory.system_vendor)
-        return {'manufacturer': system_vendor.manufacturer, 'productName': system_vendor.product_name, 'serialNumber': system_vendor.serial_number}
+        system_vendor =self._inventory['system_vendor']
+        return {'manufacturer': system_vendor.get('manufacturer', ''), 'productName': system_vendor.get('product_name', ''), 'serialNumber': system_vendor.get('serial_number', None)}
 
     def is_role(self, role):
-        return self._host.role == role
+        return self._host['role'] == role
 
     def _get_network_interface_ip(self, interface):
-        if len(interface.ipv4_addresses) > 0:
-            return interface.ipv4_addresses[0].split("/")[0]
-        if len(interface.ipv6_addresses) > 0:
-            return interface.ipv6_addresses[0].split("/")[0]
+        ipv4_addresses = interface.get('ipv4_addresses', [])
+        if len(ipv4_addresses) > 0:
+            return ipv4_addresses[0].split("/")[0]
+        ipv6_addresses = interface.get('ipv6_addresses', [])
+        if len(ipv6_addresses) > 0:
+            return ipv6_addresses[0].split("/")[0]
         return " "
 
 
